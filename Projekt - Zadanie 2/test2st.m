a94855e68ad3a7dace52ed51b4d7c64851f735ab
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-01-09 02:19:46 +0100

    Precise coefficients
    For Ralston's method

--- Projekt - Zadanie 2/test2st.m
+++ Projekt - Zadanie 2/test2st.m
@@ -6,8 +6,9 @@ r = 50;
 a = { @(x) 0, @(x) 1, @(x) 1  };
 b = @(x) x;
 Y0 = [ 1 1 ]; % x0 y(x0) y'(x0) y''(x0) ...  y^(m-1)
-[~,yv1] = P2Z49_MWY_RungeKutta4v1(l, r, a, b, Y0, N, Classic);
-[x,yv2] = P2Z49_MWY_RungeKutta4(l, r, a, b, [0 Y0], N, Classic);
+[~,yv1] = P2Z49_MWY_RungeKutta4v1(l, r, a, b, Y0, N, Ralston);
+[x,yv2] = P2Z49_MWY_RungeKutta4(l, r, a, b, [0 Y0], N, Ralston);
+% z jakiegoś powodu tutaj lepiej sprawdzała się metoda klasyczna
 yprec = 0.5*x.^2 - x - 2*exp(-x) + 3;
 
 figure(2);
