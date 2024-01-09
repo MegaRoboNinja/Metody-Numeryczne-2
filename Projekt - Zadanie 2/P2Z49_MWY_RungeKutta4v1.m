dfc2dbab973028b65ae66971a59519ebff0df377
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-01-09 02:02:58 +0100

    THE FIX
    Looks like there were only some small typos and stuff but I also set a small N.

--- Projekt - Zadanie 2/P2Z49_MWY_RungeKutta4v1.m
+++ Projekt - Zadanie 2/P2Z49_MWY_RungeKutta4v1.m
@@ -1,4 +1,4 @@
-function [x, y] = P2Z49_MWY_RungeKutta4(l, r, a, b, Y0, N, RK)
+function [x, y] = P2Z49_MWY_RungeKutta4v1(l, r, a, b, Y0, N, RK)
 % Projekt 2, zadanie 49
 % Miłosz Wysocki, 324572
 %
@@ -17,7 +17,7 @@ function [x, y] = P2Z49_MWY_RungeKutta4(l, r, a, b, Y0, N, RK)
 %   Y0 - wektor zwierający wartości punkt x0 i początkowe funkcji y i jej
 %        pochodnych występujących w równaniu, w punkcie x0. Jest on
 %        postaci:
-%               [ x0 y(x0), y'(x0), y''(x0) ... y^(m-1)(x0)]^Ts
+%               [ y(x0), y'(x0), y''(x0) ... y^(m-1)(x0)]^Ts
 %   N  - liczba punktów, w których zostanie przybliżona wartość y(x)
 %   RK - uchwyt do funkcji zwracającej tablicę butchera definiująca metodę
 %        jaką należy zastosować.
@@ -39,7 +39,7 @@ A = B(:,2:end);
 % Prealokacja wektorów wynikowych
 x = linspace(l, r, N);
 y = zeros(N, 1);
-y(1) = Y0(2);
+y(1) = Y0(1);
 
 Ylast = Y0;
 
@@ -47,41 +47,40 @@ for k = 2:N % w notatkach wyznacza się k+1 więc wszystko jest przesunięte
     % długość kroku
     h = x(k) - x(k-1);
 
-    K0 = F(Ylast);
-    K1 = F(Ylast + h*A(2,1)*K0);
-    K2 = F(Ylast + h*(A(3,1)*K0 + A(3,2)*K1));
-    K3 = F(Ylast + h*(A(4,1)*K0 + A(4,2)*K1 + A(4,3)*K2));
+    K0 = F(x(k), Ylast);
+    K1 = F(x(k), Ylast + h*A(2,1)*K0);
+    K2 = F(x(k), Ylast + h*(A(3,1)*K0 + A(3,2)*K1));
+    K3 = F(x(k), Ylast + h*(A(4,1)*K0 + A(4,2)*K1 + A(4,3)*K2));
 
     Yk = Ylast + h * (C(1)*K0 + C(2)*K1 + C(3)*K2 + C(4)*K3);
     
     % zapisanie kolejnego przybliżenia
-    y(k) = Yk(2);
+    y(k) = Yk(1);
 
     Ylast = Yk;
 end
 
-function Res = F(Y)
+function Res = F(x, Y)
 % Najwyższy rząd  pochodnej (potrzebny funkcji f)
 m = length(a) - 1;
 
-if length(Y) >= 3
-    Res = [1 Y(3:end) f(Y)]; 
+if length(Y) >= 2
+    Res = [ Y(2:end) f(x, Y)]; 
 else
-    Res = [1 f(Y)];
+    Res = [ f(x, Y) ];
 end
 
-function res = f(Y)
+function res = f(x, Y)
     % Wynacza funkcję f
-    % Y(1) to punkt x
-    res = b(Y(1));
+    res = b(x);
     for i = 1:m
         ai = a{i};
-        res = res - ai(Y(1))*Y(i+1);
+        res = res - ai(x)*Y(i);
     end
     am = a{m+1};
-    res = res * 1/(am(Y(1)));
+    res = res * 1/(am(x));
 end % function f
 end % function F
 
 
-end % function P2Z49_MWY_RungeKutta4
+end % function P2Z49_MWY_RungeKutta4v1
